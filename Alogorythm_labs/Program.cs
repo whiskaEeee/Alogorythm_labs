853122ef1404e98e9ee7e09bdedcd8815409bf3e
whiskaEeee <grishakovdima456@gmail.com>
2024-04-16 21:44:53 +0300

    giude updated

--- Alogorythm_labs/Program.cs
+++ Alogorythm_labs/Program.cs
@@ -1,4 +1,5 @@
-﻿using Alogorythm_labs.labs.lab_1;
+﻿using Alogorythm_labs.lab_1;
+using Alogorythm_labs.labs.lab_1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,8 @@ using System.Threading.Tasks;
 
 
 /*
- * 
+ *      How to use:
+ *          in line "Program_5_4_1.Main(args)" change "Program_5_4_1" to the filename you want to start; 
  *                              
             ──────▄▀▄─────▄▀▄
             ─────▄█░░▀▀▀▀▀░░█▄
@@ -20,11 +22,12 @@ using System.Threading.Tasks;
  *          Часть 1    Лр 1    Задание 8        Program_8_1_1
  *          Часть 1    Лр 2    Задание 6        Program_6_2_1
  *      Лр 2:
- *          Часть 1    Лр 3    Задание 3        Program_3_1_1
- *          Часть 1    Лр 3    Задание 4        Program_3_1_1
- *          Часть 1    Лр 4    Задание 4        Program_3_1_1
- *          Часть 1    Лр 4    Задание 5        Program_3_1_1
- *          Часть 1    Лр 4    Задание 6        Program_3_1_1
+ *          Часть 1    Лр 3    Задание 3        Program_3_3_1
+ *          Часть 1    Лр 3    Задание 4        Program_4_3_1
+ *          Часть 1    Лр 4    Задание 4        Program_4_4_1 алгоритм Краскала
+ *          Часть 1    Лр 4    Задание 4        Program_5_4_1 алгоритм Прима
+ *          Часть 1    Лр 4    Задание 5        Program_6_4_1
+ *          Часть 1    Лр 4    Задание 6        Program_7_4_1
  *      Лр 3:
  *          Часть 1    Лр 5    Задание 1        Program_3_1_1
  *
@@ -37,7 +40,7 @@ namespace Alogorythm_labs
     {
         private static void Main(string[] args)
         {
-            Program_6_2_1.Main(args);
+            Program_8_1_1.Main(args);
         }
     }
 }
