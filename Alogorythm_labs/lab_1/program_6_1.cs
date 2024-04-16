9c15e16834ea5621a4a963fefaf33bf7eda92758
whiskaEeee <grishakovdima456@gmail.com>
2024-04-16 18:47:04 +0300

    Добавьте файлы проекта.

---
+++ Alogorythm_labs/lab_1/program_6_1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alogorythm_labs.lab_1
+{
+    internal class program_6_1
+    {
+    }
+}
