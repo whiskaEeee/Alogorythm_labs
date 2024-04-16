e826dbcf4287e863acec5176d19c1caa0fce9f01
whiskaEeee <grishakovdima456@gmail.com>
2024-04-16 19:25:20 +0300

    test change

--- Alogorythm_labs/Program.cs
+++ Alogorythm_labs/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
+﻿using Alogorythm_labs.lab_1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*
+ * 
+ *                              
+            ──────▄▀▄─────▄▀▄
+            ─────▄█░░▀▀▀▀▀░░█▄
+            ─▄▄──█░░░░░░░░░░░█──▄▄
+            █▄▄█─█░░▀░░┬░░▀░░█─█▄▄█
+ * 
+ *                   README
+ *      Лр 1:
+ *          Часть 1    Лр 1    Задание 3
+ *          Часть 1    Лр 1    Задание 8
+ *          Часть 1    Лр 2    Задание 6
+ *      Лр 2:
+ *          Часть 1    Лр 3    Задание 3
+ *          Часть 1    Лр 3    Задание 4
+ *          Часть 1    Лр 4    Задание 4
+ *          Часть 1    Лр 4    Задание 5
+ *          Часть 1    Лр 4    Задание 6
+ *
+ * 
+ * 
+ */
 namespace Alogorythm_labs
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Program_3_1_1.Main(args);
         }
     }
 }
---
+++ Alogorythm_labs/labs/lab_1/Program_3_1_1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alogorythm_labs.lab_1
+{
+    internal class Program_3_1_1
+    {
+
+        public static void Main(string[] args)
+        {
+            List<int> list = GenerateList(20);
+            PrintList(list);
+
+            Console.ReadKey();
+        }
+
+        static void ShakerSortAlgorithm(List<int> list)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < list.Count - 1; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        Swap(list, i, i + 1);
+                        swapped = true;
+                    }
+                }
+                if (!swapped)
+                    break;
+
+                swapped = false;
+                for (int i = list.Count - 2; i >= 0; i--)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        Swap(list, i, i + 1);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        static void Swap(List<int> list, int a, int b)
+        {
+            int temp = list[a];
+            list[a] = list[b];
+            list[b] = temp;
+        }
+
+        static void PrintList(List<int> list)
+        {
+            foreach (int num in list)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static List<int> GenerateList(int count)
+        {
+            List<int> numbers = new List<int>();
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Add(random.Next(-100, 101));
+            }
+
+            return numbers;
+        }
+    }
+
+}
